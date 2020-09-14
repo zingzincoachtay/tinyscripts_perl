f1d109701b1e8c75079b719b02d53a7332235653
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-09-14 17:07:28 -0400

    Update, no test

--- Benchmark.pl
+++ Benchmark.pl
@@ -5,7 +5,7 @@ my $current = &today(time);
 my $param = {'rep'=>int rand(1024)
             ,'set'=>10
             ,'job'=>['while']
-            ,'lap'=>{ 'for'=>[],'foreach'=>[],'foranon'=>[],'do'=>[],'while'=>[],'whileeach'=>[],'switch'=>[],'if'=>[] };
+            ,'lap'=>{ 'for'=>[],'foreach'=>[],'foranon'=>[],'do'=>[],'while'=>[],'whileeach'=>[],'strlen'=>[],'strpos'=>[],'strind'=>[],'switch'=>[],'if'=>[] };
 };
 
 # http://baishui.info/orelly/linux/cgi/ch17_01.htm
@@ -67,19 +67,23 @@ sub strenuous {
   my ($rep,$set,$job,$lap) = ($v->{'rep'},$v->{'set'},$v->{'job'},$v->{'lap'});
   # https://perldoc.perl.org/perlsyn.html#Switch-statements
   SWITCH: for(@$job) {
-        'for' && foreach(1 .. $set) { push(@{$lap->    {'for'}},    &test_for(0,shift @_) )}
-    'foreach' && foreach(1 .. $set) { push(@{$lap->{'foreach'}},&test_foreach(1,shift @_) )}
-    'foranon' && foreach(1 .. $set) { push(@{$lap->{'foranon'}},&test_foranon(1,shift @_) )}
-           'do' && foreach(1 .. $set) { push(@{$lap->       {'do'}},       &test_do(1,shift @_) )}
-        'while' && foreach(1 .. $set) { push(@{$lap->    {'while'}},    &test_while(0,shift @_) )}
-    'whileeach' && foreach(1 .. $set) { push(@{$lap->{'whileeach'}},&test_whileeach(1,shift @_) )}
-    /^length$/i && do{};
-    # array size, `length` function vs array indexing
+        'for' && foreach(1 .. $set) { push(@{$lap->    {'for'}},    &test_for(0,$rep) )}
+    'foreach' && foreach(1 .. $set) { push(@{$lap->{'foreach'}},&test_foreach(1,$rep) )}
+    'foranon' && foreach(1 .. $set) { push(@{$lap->{'foranon'}},&test_foranon(1,$rep) )}
+           'do' && foreach(1 .. $set) { push(@{$lap->       {'do'}},       &test_do(1,$rep) )}
+        'while' && foreach(1 .. $set) { push(@{$lap->    {'while'}},    &test_while(0,$rep) )}
+    'whileeach' && foreach(1 .. $set) { push(@{$lap->{'whileeach'}},&test_whileeach(1,$rep) )}
+    'arrlen' &&
+    'arrind' &&
     /^strpos$/i && do{};
     # string search, 'strpos' vs regex
     # string concat, .(dot) operator vs "(double quotation) expansion
     # conditional, serial 'if's vs 'elseif's vs switch
 
+    /^(str)?len(gth)?s?/i && foreach(1 .. $rep) {
+      push(@{$lap->{'strlen'}}, &test_strlen(&word) )};
+    /^(str)?(regex(?=len)|regex(?<len))/i && foreach(1 .. $rep) {
+      push(@{$lap->{'strlenregex'}}, &test_strlenregex(&word) )};
   }
 }
 sub test_for     { my ($k,$n)=@_; my $p=time; for($k=0;$k<$n;$k++){ &job(); } return dev(time,$p,$n); }
@@ -88,18 +92,24 @@ sub test_foranon { my ($i,$n)=@_; my $p=time; foreach ($k .. $n){ &job(); } retu
 sub test_do         { my ($j,$n)=@_; my $p=time; do{ &job();$j++; }while($j<$n); } return dev(time,$p,$n); }
 sub test_while      { my ($k,$n)=@_; my $p=time; while($k<$n){ &job();$k++; } return dev(time,$p,$n); }
 sub test_whileeach  { my ($i,$n)=@_; my %h=map {$_=>0} ($i .. $n); my $p=time; while(my($k,$v)=each(%h)){ &job(); } return dev(time,$p,$n); }
+sub test_arrlen { my ($n)=@_; my $box=&sets($n); my $p=time; length(@$box); return dev(time,$p,$n); }
+sub test_arrind { my ($n)=@_; my $box=&sets($n); my $p=time; $#$box+1; return dev(time,$p,$n); }
 
-sub test_length {	my $str="potassium"; my $loop; my ($stop) = @_;# print "$stop\n" if $stop+$stop > $upper_limit;
-  foreach $loop (1..$stop) { 1 if( length($str)==0 ); } }
-sub test_length_regex {	my $str="potassium"; my $loop; my ($stop) = @_;# print "$stop\n" if $stop+$stop > $upper_limit;
-  foreach $loop (1..$stop) { 1 if( $str!~/.+/ );  } }
 sub test_strpos {	my $str="potassium"; my $loop; my ($stop) = @_;# print "$stop\n" if $stop+$stop > $upper_limit;
   foreach $loop (1..$stop) { 1 if( index($str,'sium')>0 ); } }
 sub test_strpos_regex {	my $str="potassium"; my $loop; my ($stop) = @_;# print "$stop\n" if $stop+$stop > $upper_limit;
   foreach $loop (1..$stop) { 1 if( $str=~/sium$/ );  } }
+sub test_strlen { my ($s)=@_; return length($s); }
+sub test_strlen_regex { my ($s)=@_; my $t=substr($s,0,-1); $s=~/$t/; return pop @+; }
+sub test_strlen0 { my ($s)=@_; return 1 if length($s)==0; }
+sub test_streq0 { my ($s)=@_; return 1 if $s eq ''; }
+sub test_strregex0 { my ($s)=@_; return 1 if $s!~/./; }
+sub test_length_regex {	 }
 
 #sub job { return (rand>0) ? 1 : 0; }
 sub job { return rand; }
+sub word { return 'Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu'; }
+sub sets { my ($N)=@_; my $box[]; push(@box, &job() ) foreach (1 .. $N); return $box; }
 sub mean { return (length(@_)==0) ? -1234 : (sum(@_)/length(@_)); }
 sub sum { my $ans=0; $ans+=$_ foreach (@_); return $ans; }
 sub dev { my ($x,$xhat,$n) = @_; return ($x-$xhat)/$n; }
