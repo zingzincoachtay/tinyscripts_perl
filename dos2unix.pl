2a50a1b04f86c097bc796b514a0b72e2cf9bc2d0
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-11-27 09:27:36 -0500

    Left only dos2unix part

--- dos2unix.pl
+++ dos2unix.pl
@@ -5,6 +5,6 @@ use warnings;
 
 ### 1. dos2unix
 ###    Usefull command to convert a windows-formatted text file to the unix/linux format from the command-line. 
-#      It converts in-place ("-i" switch) the "\r\n" to "\n" characters using a regular expression.
+#.     It converts in-place ("-i" switch) the "\r\n" to "\n" characters using a regular expression.
 
 perl -pi -e's/\015\012/\012/g' $ARGV[0]
