02f0db91dd733917efcd4fdb69449fecdfbf4067
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-11-27 09:26:56 -0500

    Update mac2unix.pl

--- mac2unix.pl
+++ mac2unix.pl
@@ -5,10 +5,10 @@ use warnings;
 
 ### 2. mac2unix
 ###    Usefull command to convert a mac-formatted text file to the unix/linux format from the command-line. 
-#      It converts in-place ("-i" switch) the "\r" that is the end of line character in mac to "\n" characters using perl built-in capabilities.
+#.     It converts in-place ("-i" switch) the "\r" that is the end of line character in mac to "\n" characters using perl built-in capabilities.
 
-#perl -w015l12pi -e1 mac_formated_file.txt
+#perl -w015l12pi -e1 $ARGV[0]
 
-which is equivalent to the following but adds a new line at the end:
+#.     which is equivalent to the following but adds a new line at the end:
 
-perl -pi -e's/\015/\012/g' mac_formated_file.txt
+perl -pi -e's/\015/\012/g' $ARGV[0]
